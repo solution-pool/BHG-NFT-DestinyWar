5b5684791726f28c40bd7de3facabed51e37f755
zabandr <88914566+timoflask@users.noreply.github.com>
2021-11-11 23:17:06 +0300

    inventory page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,40 @@
 import './Inventory.scss';
+import dwar from '../../assets/img/inventory/11.png';
+import mount from '../../assets/img/inventory/22.png';
+import pet from '../../assets/img/inventory/33.png';
 
 export const Inventory = () => {
     return (
-        <section className="inventory">
-            
+        <section className="inventory container">
+            <div className="inventory__title">
+                My Inventory
+            </div>
+
+            <div className="inventory__items">
+                <div className="inventory__items__dwar">
+                    <div className="inventory__items__title">1 DWAR</div>
+                    <div className="inventory__items__pic">
+                        <span>#123123123</span>
+                        <img alt="pic" src={dwar}></img>
+                    </div>
+                </div>
+
+                <div className="inventory__items__mount">
+                    <div className="inventory__items__title">1 MOUNT</div>
+                    <div className="inventory__items__pic">
+                        <span>#123123123</span>
+                        <img alt="pic" src={mount}></img>
+                    </div>
+                </div>
+
+                <div className="inventory__items__pet">
+                    <div className="inventory__items__title">1 PET</div>
+                    <div className="inventory__items__pic">
+                        <span>#123123123</span>
+                        <img alt="pic" src={pet}></img>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -10,7 +10,7 @@ export const topMenu = () => {
         <section className="topMenu container">
             <div className="topMenu__menus">
                 <div className="topMenu__menus__item">
-                    <a href="#javascript;">HOME</a>
+                    <Link to="/">HOME</Link>
                 </div>
                 <div className="topMenu__menus__item">
                     <Link to="/inventory">INVENTORY</Link>
@@ -31,8 +31,8 @@ export const topMenu = () => {
                     <svg stroke="currentColor" fill="#ffffff" strokeWidth="0" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
                 </button>
                 <div className="topMenu__dropDownMenu__content">
-                    <a href="#javascript;">HOME</a>
-                    <a href="#javascript;">INVENTORY</a>
+                    <Link to="/">HOME</Link>
+                    <Link to="/inventory">INVENTORY</Link>
                     <a href="#javascript;">WHITEPAPER</a>
                     <a href="#javascript;">GAME-WIKIPEDIA</a>
                     <a href="#javascript;">MARKETPLACE</a>
