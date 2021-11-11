f15ae47ab9b2419a1c09495377637217ac8faeec
zabandr <88914566+timoflask@users.noreply.github.com>
2021-11-11 05:56:40 +0300

    roadMap section

--- src/components/RoadMap/RoadMap.js
+++ src/components/RoadMap/RoadMap.js
@@ -1,9 +1,30 @@
 import './RoadMap.scss';
 
+const imageCount = 9;
+const images = require.context('../../assets/img', true);
+const roadMapImages = [];
+
+for( let i = 1; i <= imageCount ; i ++ ) {
+    const image = images(`./roadmap${i}.png`);
+    roadMapImages.push(image);
+}
+
 export const RoadMap = () => {
     return (
-        <section>
+        <section className="roadMap container">
+            <div className="roadMap__title">
+                ROADMAP
+            </div>
 
+            <div className="roadMap__content">
+                {
+                    roadMapImages.map((item, index) => (
+                        <div className={`roadMap__content__item ${ index % 2 == 0 ? 'toLeft' : 'toRight' }`} key={index}>
+                            <img alt="pic" src={item.default}></img>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
